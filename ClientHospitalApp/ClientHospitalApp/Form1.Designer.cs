8bdfbd0a5ec4f252aa491a25cbbb0f3fdb46b7c0
annalogozinskaja <annalogozinskaja@gmail.com>
2020-10-21 21:04:24 +0300

    work with DevExpress

--- ClientHospitalApp/ClientHospitalApp/Form1.Designer.cs
+++ ClientHospitalApp/ClientHospitalApp/Form1.Designer.cs
@@ -28,133 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.buttonUpdate = new System.Windows.Forms.Button();
-            this.buttonAdd = new System.Windows.Forms.Button();
-            this.buttonDelete = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle9;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(713, 304);
-            this.dataGridView1.TabIndex = 11;
-            // 
-            // buttonUpdate
-            // 
-            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonUpdate.Location = new System.Drawing.Point(172, 345);
-            this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(90, 35);
-            this.buttonUpdate.TabIndex = 12;
-            this.buttonUpdate.Text = "Update";
-            this.buttonUpdate.UseVisualStyleBackColor = true;
-            // 
-            // buttonAdd
-            // 
-            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonAdd.Location = new System.Drawing.Point(328, 345);
-            this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(90, 35);
-            this.buttonAdd.TabIndex = 13;
-            this.buttonAdd.Text = "Add";
-            this.buttonAdd.UseVisualStyleBackColor = true;
-            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
-            // 
-            // buttonDelete
-            // 
-            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonDelete.Location = new System.Drawing.Point(477, 345);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(90, 35);
-            this.buttonDelete.TabIndex = 14;
-            this.buttonDelete.Text = "Delete";
-            this.buttonDelete.UseVisualStyleBackColor = true;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "ID_Patient";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Lastname";
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 200;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Firstname";
-            this.Column3.Name = "Column3";
-            this.Column3.Width = 200;
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "DOB";
-            this.Column4.Name = "Column4";
-            this.Column4.Width = 170;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 398);
-            this.Controls.Add(this.buttonDelete);
-            this.Controls.Add(this.buttonAdd);
-            this.Controls.Add(this.buttonUpdate);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(751, 535);
             this.Name = "Form1";
             this.Text = "List of patients";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.Button buttonAdd;
-        private System.Windows.Forms.Button buttonDelete;
     }
 }
 
--- ClientHospitalApp/ClientHospitalApp/Form1.cs
+++ ClientHospitalApp/ClientHospitalApp/Form1.cs
@@ -10,6 +10,11 @@ using System.Windows.Forms;
 using ClientHospitalApp.Views;
 using ClientHospitalApp.Presenters;
 using ClientHospitalApp.ServiceReferenceDAOLayer;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Utils.Extensions;
+using DevExpress.XtraEditors;
 
 namespace ClientHospitalApp
 {
@@ -61,31 +66,72 @@ namespace ClientHospitalApp
             PatientPresenter presenter = new PatientPresenter();
             presenter.GetAllPatientsFromModel();
 
-            foreach (IPatient item in presenter.patientViewList)
-            {
-                dataGridView1.Rows.Add(item.ID_PatientText, item.LastnameText, item.FirstnameText, item.DOBText);
-            }
+            GridControl gridControl1 = new GridControl();
+            gridControl1.Parent = this;
+            //gridControl1.Dock = DockStyle.Fill;
+            gridControl1.DataSource = presenter.patientViewList;
+
+            GridView gridView1 = gridControl1.MainView as GridView;
+            GridColumn colID = gridView1.Columns["ID_PatientText"];
+            GridColumn colLastname = gridView1.Columns["LastnameText"];
+            GridColumn colFirstname = gridView1.Columns["FirstnameText"];
+            GridColumn colDOB = gridView1.Columns["DOBText"];
+            GridColumn colSSN = gridView1.Columns["SSNText"];
+            GridColumn colGender = gridView1.Columns["ID_GenderText"];
+
+            colLastname.Name = "Lastname";
+            colFirstname.Name = "Firstname";
+            colDOB.Name = "Data of birth";
+            colSSN.Name = "SSN";
+
+            colID.Visible = false;
+            colGender.Visible = false;
+
+            colLastname.SortOrder = DevExpress.Data.ColumnSortOrder.Ascending;
+            gridView1.OptionsView.ShowGroupedColumns = true;
+            gridView1.ExpandAllGroups();
+            gridView1.ActiveFilterString = "[RequiredDate]>= #" + DateTime.Today.ToString() + "#";
+            gridView1.OptionsBehavior.Editable = false;
+
+            gridView1.FocusedRowHandle = 0;
+            gridView1.FocusedColumn = colID;
+
+            colLastname.BestFit();
+            colGender.BestFit();
+
+
+            SimpleButton buttonOK = new SimpleButton();
+            buttonOK.Parent = this;
+            buttonOK.Text = "OK";
+            buttonOK.Width = 100;
+            buttonOK.Height = 50;
+            buttonOK.Location= new Point(250,250);
+            buttonOK.Visible = true;
+            buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+
+
+
         }
 
-        private void buttonAdd_Click(object sender, EventArgs e)
-        {
-            Form2 F = new Form2();
-            F.Text = "Add patient";
+        //private void buttonAdd_Click(object sender, EventArgs e)
+        //{
+        //    Form2 F = new Form2();
+        //    F.Text = "Add patient";
 
-            DialogResult res = F.ShowDialog();
+        //    DialogResult res = F.ShowDialog();
 
-            if (res == DialogResult.OK)
-            {
-                try
-                {
+        //    if (res == DialogResult.OK)
+        //    {
+        //        try
+        //        {
                     
 
-                }
-                catch (Exception s)
-                {
-                    Console.WriteLine("Error ({0} : {1}", s.GetType().Name, s.Message);
-                }
-            }
-        }
+        //        }
+        //        catch (Exception s)
+        //        {
+        //            Console.WriteLine("Error ({0} : {1}", s.GetType().Name, s.Message);
+        //        }
+        //    }
+        //}
     }
 }
